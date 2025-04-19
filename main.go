4eaed91c9a0927501ed1f26c49b9c66c464f10d3
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-04-19 17:55:23 -0600

    Finished regex identifier tokens

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(getTokensFromLine("int main = 0;"))
+	fmt.Println(getTokensFromLine("String hello = \"Hello, World!\"; // This is a comment"));
+	fmt.Println()
+	fmt.Println(getTokensFromLine("int x = 42;"));
+	fmt.Println()
+	fmt.Println(getTokensFromLine("if (x > 0) { return x; }"));
+	fmt.Println()
+	fmt.Println(getTokensFromLine("char c = 'a';"));
+	fmt.Println()
+	fmt.Println(getTokensFromLine("boolean flag = true;"));
+	fmt.Println()
+	fmt.Println(getTokensFromLine("for (int i = 0; i < 10; i++) { System.out.println(i); }"));
 }
